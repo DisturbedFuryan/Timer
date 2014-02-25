65078aea68807fe8b5d05d85ceeeefab3a63bcac
Marcin Rainka <marcin.rainka@gmail.com>
2014-02-25 14:46:41 +0100

    Testing code.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,27 @@
+#include "Timer.hpp"
+#include <iostream>
+
+using namespace std;
+
+
+inline void Pause() {
+    cout << "Press ENTER to continue...";
+    cin.get();
+}
+
+
 //**************************************************************************************************
 int main() {
+    Timer tmr;
+
+    Pause();
+    tmr.Start();
+
+    Pause();
+    tmr.Stop();
+
+    cout << "Elapsed time: " << ( tmr.GetTime() / 1000.0 ) << " s\n";
+
     return 0;
 }
 //**************************************************************************************************
